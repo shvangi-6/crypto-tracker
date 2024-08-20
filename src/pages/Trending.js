958229b7a363e775843cb7841e90196a15ec78f2
shvangi-6 <shivangichaubey292@gmail.com>
2024-08-20 10:36:43 +0530

    first commit

---
+++ src/pages/Trending.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import Cards from "../components/Cards";
+function Trending() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (data.length === 0) {
+    return <div>Dummy UI Loading </div>;
+  } else {
+    return (
+      <div>
+        <Cards apiData={data} />
+      </div>
+    );
+  }
+}
+
+export default Trending;
